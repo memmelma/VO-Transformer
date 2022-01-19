a7465d08193a1356deb41bb29f32b3cb98ea1eee
memmelma <marius.memmel@gmx.net>
2022-01-19 10:40:17 +0000

    extend to wandb

--- pointnav_vo/utils/tensorboard_utils.py
+++ pointnav_vo/utils/tensorboard_utils.py
@@ -4,15 +4,16 @@
 # This source code is licensed under the MIT license found in the
 # LICENSE file in the root directory of this source tree.
 
+import os
+import wandb
 from typing import Any
-
 import numpy as np
 import torch
 from torch.utils.tensorboard import SummaryWriter
 
 
 class TensorboardWriter:
-    def __init__(self, log_dir: str, *args: Any, **kwargs: Any):
+    def __init__(self, log_dir: str, config: Any, use_wandb=True, *args: Any, **kwargs: Any):
         r"""A Wrapper for tensorboard SummaryWriter. It creates a dummy writer
         when log_dir is empty string or None. It also has functionality that
         generates tb video directly from numpy images.
@@ -23,23 +24,34 @@ class TensorboardWriter:
             *args: Additional positional args for SummaryWriter
             **kwargs: Additional keyword args for SummaryWriter
         """
-        self.writer = None
-        if log_dir is not None and len(log_dir) > 0:
-            self.writer = SummaryWriter(log_dir, *args, **kwargs)
-
-    def __getattr__(self, item):
-        if self.writer:
-            return self.writer.__getattribute__(item)
+        
+        self.use_wandb = use_wandb
+        if self.use_wandb:
+            os.system("wandb login --relogin $WANDB_API_KEY")
+            self.run = wandb.init(project="vo", entity="memmelma", mode="disabled" if config.TASK_CONFIG.DATASET.SPLIT != 'train' else None, reinit=True)
+            wandb.config = vars(config)
         else:
-            return lambda *args, **kwargs: None
+            self.writer = None
+            if log_dir is not None and len(log_dir) > 0:
+                self.writer = SummaryWriter(log_dir, *args, **kwargs)
+
+    # def __getattr__(self, item):
+    #     if self.writer:
+    #         return self.writer.__getattribute__(item)
+    #     else:
+    #         return lambda *args, **kwargs: None
 
     def __enter__(self):
         return self
 
     def __exit__(self, exc_type, exc_val, exc_tb):
-        if self.writer:
+        # if self.writer:
+        #     self.writer.close()
+        if self.use_wandb:
+            self.run.finish()
+        else:
             self.writer.close()
-
+            
     def add_video_from_np_images(
         self, video_name: str, step_idx: int, images: np.ndarray, fps: int = 10
     ) -> None:
@@ -54,11 +66,34 @@ class TensorboardWriter:
         Returns:
             None.
         """
-        if not self.writer:
+        if not self.writer or self.use_wandb:
             return
+        
         # initial shape of np.ndarray list: N * (H, W, 3)
         frame_tensors = [torch.from_numpy(np_arr).unsqueeze(0) for np_arr in images]
         video_tensor = torch.cat(tuple(frame_tensors))
         video_tensor = video_tensor.permute(0, 3, 1, 2).unsqueeze(0)
         # final shape of video tensor: (1, n, 3, H, W)
-        self.writer.add_video(video_name, video_tensor, fps=fps, global_step=step_idx)
+
+        if self.use_wandb:
+            video = wandb.Video(data_or_path=video_tensor, caption=video_name, fps=fps)
+            wandb.log({video_name: video}, step=int(step_idx))
+        else:
+            self.writer.add_video(video_name, video_tensor, fps=fps, global_step=step_idx)
+
+    def add_image(
+        self, descriptor: str, img: Any, global_step: int, dataformats=None, *args: Any, **kwargs: Any
+    ) -> None:
+        if self.use_wandb:
+            img = wandb.Image(img, caption="")
+            wandb.log({descriptor: img}, step=int(global_step))
+        else:
+            self.writer.add_image(descriptor, value, global_step)
+
+    def add_scalar(
+        self, descriptor: str, value: Any, global_step: int, *args: Any, **kwargs: Any
+        ) -> None:
+        if self.use_wandb:
+            wandb.log({descriptor: value}, step=int(global_step))
+        else:
+            self.writer.add_scalar(descriptor, value, global_step)
