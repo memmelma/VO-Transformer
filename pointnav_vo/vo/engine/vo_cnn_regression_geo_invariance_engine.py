6cde865a978a7c99533d6558d2c11ae342e4a59c
memmelma <marius.memmel@gmx.net>
2022-02-22 17:14:21 +0000

    update optimizer initialization

--- pointnav_vo/vo/engine/vo_cnn_regression_geo_invariance_engine.py
+++ pointnav_vo/vo/engine/vo_cnn_regression_geo_invariance_engine.py
@@ -5,7 +5,7 @@ import torch.optim as optim
 from pointnav_vo.utils.baseline_registry import baseline_registry
 from pointnav_vo.vo.engine.vo_ddp_regression_geo_invariance_engine import VODDPRegressionGeometricInvarianceEngine
 
-ENGINE_NAME = "vo_cnn_regression_geo_invariance_engine_ddp"
+ENGINE_NAME = "vo_cnn_regression_geo_invariance_engine"
 
 @baseline_registry.register_vo_engine(name=ENGINE_NAME)
 class VOCNNRegressionGeometricInvarianceEngine(VODDPRegressionGeometricInvarianceEngine):
@@ -14,21 +14,41 @@ class VOCNNRegressionGeometricInvarianceEngine(VODDPRegressionGeometricInvarianc
         super().__init__(config, run_type, ENGINE_NAME, verbose)
 
     def _set_up_optimizer(self):
-
+        
+        self.optimizer = {}
         self.optimizer_dict = {'adam': optim.Adam, 'adamw': optim.AdamW}
 
-        for act in self._act_list:
-            self.optimizer[act] = self.optimizer_dict[self.config.VO.TRAIN.optim](
-                list(
-                    filter(lambda p: p.requires_grad, self.vo_model[act].parameters())
-                ),
-                lr=self.config.VO.TRAIN.lr,
-                eps=self.config.VO.TRAIN.eps,
-                weight_decay=self.config.VO.TRAIN.weight_decay,
-            )
+        if self.config.VO.MODEL.pretrain_backbone != 'None' and self.config.VO.MODEL.train_backbone:
+            for act in self._act_list:
+                self.optimizer[act] = self.optimizer_dict[self.config.VO.TRAIN.optim](
+                    [{'params': self.vo_model[act].visual_encoder.parameters(), 'lr': self.config.VO.TRAIN.backbone_lr},
+                    {'params': self.vo_model[act].head.parameters()}],
+                    lr=self.config.VO.TRAIN.lr,
+                    eps=self.config.VO.TRAIN.eps,
+                    weight_decay=self.config.VO.TRAIN.weight_decay,
+                )
+
+        else:
+            for act in self._act_list:
+                if not self.config.VO.MODEL.train_backbone:
+                    # freeze backbone
+                    for p in self.vo_model[act].backbone.parameters():
+                        p.requires_grad = False
+
+                self.optimizer[act] = self.optimizer_dict[self.config.VO.TRAIN.optim](
+                    list(
+                        filter(lambda p: p.requires_grad, self.vo_model[act].parameters())
+                    ),
+                    lr=self.config.VO.TRAIN.lr,
+                    eps=self.config.VO.TRAIN.eps,
+                    weight_decay=self.config.VO.TRAIN.weight_decay,
+                )
 
         if self.config.RESUME_TRAIN:
             resume_ckpt = torch.load(self.config.RESUME_STATE_FILE)
             for act in self._act_list:
                 self.optimizer[act].load_state_dict(resume_ckpt["optim_states"][act])
 
+
+
+        
--- pointnav_vo/vo/engine/vo_transformer_regression_geo_invariance_engine.py
+++ pointnav_vo/vo/engine/vo_transformer_regression_geo_invariance_engine.py
@@ -18,15 +18,31 @@ class VOTransformerRegressionGeometricInvarianceEngine(VODDPRegressionGeometricI
         self.optimizer = {}
         self.optimizer_dict = {'adam': optim.Adam, 'adamw': optim.AdamW}
 
-        for act in self._act_list:
-            self.optimizer[act] = self.optimizer_dict[self.config.VO.TRAIN.optim](
-                [{'params': self.vo_model[act].vit.parameters(), 'lr': self.config.VO.TRAIN.backbone_lr / 10},
-                {'params': self.vo_model[act].running_mean_and_var.parameters()},
-                {'params': self.vo_model[act].head.parameters()}],
-                lr=self.config.VO.TRAIN.lr,
-                eps=self.config.VO.TRAIN.eps,
-                weight_decay=self.config.VO.TRAIN.weight_decay,
-            )
+        if self.config.VO.MODEL.pretrain_backbone != 'None' and self.config.VO.MODEL.train_backbone:
+            for act in self._act_list:
+                self.optimizer[act] = self.optimizer_dict[self.config.VO.TRAIN.optim](
+                    [{'params': self.vo_model[act].vit.parameters(), 'lr': self.config.VO.TRAIN.backbone_lr},
+                    {'params': self.vo_model[act].head.parameters()}],
+                    lr=self.config.VO.TRAIN.lr,
+                    eps=self.config.VO.TRAIN.eps,
+                    weight_decay=self.config.VO.TRAIN.weight_decay,
+                )
+
+        else:
+            for act in self._act_list:
+                if not self.config.VO.MODEL.train_backbone:
+                    # freeze backbone
+                    for p in self.vo_model[act].backbone.parameters():
+                        p.requires_grad = False
+
+                self.optimizer[act] = self.optimizer_dict[self.config.VO.TRAIN.optim](
+                    list(
+                        filter(lambda p: p.requires_grad, self.vo_model[act].parameters())
+                    ),
+                    lr=self.config.VO.TRAIN.lr,
+                    eps=self.config.VO.TRAIN.eps,
+                    weight_decay=self.config.VO.TRAIN.weight_decay,
+                )
 
         if self.config.RESUME_TRAIN:
             resume_ckpt = torch.load(self.config.RESUME_STATE_FILE)
