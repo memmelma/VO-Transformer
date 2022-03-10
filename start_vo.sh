0e64db7a3d0bb903f84018a045d29fe580af41a2
memmelma <marius.memmel@gmx.net>
2022-03-10 18:30:54 +0000

    generate new data

--- start_vo.sh
+++ start_vo.sh
@@ -5,10 +5,10 @@ export PYTHONPATH=${POINTNAV_VO_ROOT}:$PYTHONPATH && \
 export NUMBA_NUM_THREADS=1 && \
 export NUMBA_THREADING_LAYER=workqueue && \
 # apply habitat sim fix
-# cp /datasets/home/memmel/PointNav-VO/WindowlessContext.cpp /home/memmel/habitat-sim/src/esp/gfx/WindowlessContext.cpp && \
-# cd /home/memmel/habitat-sim && \
-# python setup.py install --headless &&\
-# cd ${POINTNAV_VO_ROOT} && \
+cp /datasets/home/memmel/PointNav-VO/WindowlessContext.cpp /home/memmel/habitat-sim/src/esp/gfx/WindowlessContext.cpp && \
+cd /home/memmel/habitat-sim && \
+python setup.py install --headless &&\
+cd ${POINTNAV_VO_ROOT} && \
 # login to git
 sh login_git.sh
 # exec script
